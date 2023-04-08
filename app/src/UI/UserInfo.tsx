6ea5a8fd5b3d1d5a914da72f44bff7beb46f5665
Ivan Zennaro <ivanzen@live.it>
2023-04-08 20:19:04 +0200

    Some code clean up in useTransactionManager

--- app/src/UI/UserInfo.tsx
+++ app/src/UI/UserInfo.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
 import { useMyFunction } from "./useTransactionManager";
 import Button from "@mui/material/Button";
 
 export default function UserInfo() {
-  /*const { activeAddress, signTransactions } = useWalletUI();*/
-
-  const getAddress = useMyFunction();
+  const { donor_buy_token } = useMyFunction();
 
   const myCall = () => {
-    getAddress().then(() => console.log("eseguito"));
+    donor_buy_token(1).then(() => console.log("eseguito"));
   };
 
   return (
--- app/src/UI/useTransactionManager.js
+++ app/src/UI/useTransactionManager.js
@@ -11,8 +11,7 @@ const algodServer = 'https://testnet-algorand.api.purestake.io/ps2'
 const algodPort = '';
 
 const client = new algosdk.Algodv2(algodToken, algodServer, algodPort);
-
-const jsonPath = "crt.json";
+const contract = new algosdk.ABIContract(crt);
 
 // Da compilare dopo step doDeploy
 let smartContractAddress = "NC6BZAISGFL2OXU6FSN7MQH6KOCAKOWUTVLJ32ANJSOKHZTZ6QNBMFP2MU"; 
@@ -21,7 +20,6 @@ let assetId = 170690482;
 let usdc_id = 67395862;
 
 import { useCallback } from "react";
-import internal from "stream";
 
 export default function useTransactionManager(){
   return 0;
@@ -29,18 +27,14 @@ export default function useTransactionManager(){
 
 export function useMyFunction(){
 
-  const { activeAddress, signTransactions, signer } = useWalletUI();
+  const { activeAddress, signer } = useWalletUI();
 
-  const getAddress = useCallback(async() => {
+  const donor_buy_token = useCallback(async(amount) => {
     
     const atc = new algosdk.AtomicTransactionComposer();
     const sp = await client.getTransactionParams().do();
-
     sp.fee = 10;
 
-   //SyntaxError: "[object Object]" is not valid JSON
-    const contract = new algosdk.ABIContract(crt);
-  
     const commonParams = {
       appID: appId,
       sender: activeAddress,
@@ -48,25 +42,21 @@ export function useMyFunction(){
       signer: signer,
     };
   
-
     const txn = {
       txn: algosdk.makeAssetTransferTxnWithSuggestedParams(
         activeAddress, 
         smartContractAddress, 
         undefined, undefined,
-        1, undefined, usdc_id, sp),
+        amount, undefined, usdc_id, sp),
         signer: signer
       };
-   
-    /*const encodedTransaction = algosdk.encodeUnsignedTransaction(txn)
-    const signedTransactions = await signTransactions([encodedTransaction])*/
-  
+     
     atc.addMethodCall({
       method: getMethodByName("donor_buy_token", contract),
       methodArgs: [txn, assetId],
       ...commonParams,
     });
-  
+
     const result = await atc.execute(client, 2);
     for (const idx in result.methodResults) {
       console.log(result.methodResults[idx]);
@@ -76,7 +66,7 @@ export function useMyFunction(){
   
   });
 
-  return getAddress;
+  return { donor_buy_token };
 
 }
 
